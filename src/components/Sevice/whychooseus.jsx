1fd5d39543384df3bf05d8dd491759f63ccb3021
Savinrai <salvinrai24@gmail.com>
2024-02-16 15:16:35 +0530

    service page modified - salvin

--- src/components/Sevice/whychooseus.jsx
+++ src/components/Sevice/whychooseus.jsx
@@ -13,12 +13,10 @@ const WhyChooseUs = () => {
                 </div>
                 <div className="w-full md:w-2/5 lg:w-1/2 px-4 text-center md:text-left lg:pl-12">
                     <h3 className="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
-                        Simple integration
+                    Expertise: 
                     </h3>
                     <p className="sm:text-lg mt-6">
-                        Use the LocaleData gem to download translations directly into your Ruby on Rails
-                        projects using the provided command line interface. Just create a project and follow
-                        the step-by-step instructions.
+                    Benefit from our team's extensive expertise in application development, marketing strategies, and software maintenance, ensuring that every aspect of your digital presence is handled with precision and proficiency.
                     </p>
                 </div>
             </div>
@@ -29,13 +27,12 @@ const WhyChooseUs = () => {
                 </div>
                 <div className="w-full md:w-2/5 lg:w-1/2 px-4 md:order-first text-center md:text-left lg:pr-12">
                     <h3 className="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
-                        Easy collaboration
+                    Integrated Solutions: 
                     </h3>
                     <p className="sm:text-lg mt-6">
-                        All LocaleData projects are private. Each project can have multiple collaborators
-                        with different roles and access permissions. You determine who can see and edit
-                        your translations. Just add admins, developers, translators and configure their
-                        access rights.
+                    We offer comprehensive solutions that cover the entire lifecycle of 
+                    your digital assets, from initial development to ongoing maintenance and marketing efforts,
+                     providing a seamless and cohesive experience for your business.
                     </p>
                 </div>
             </div>
@@ -46,13 +43,12 @@ const WhyChooseUs = () => {
                 </div>
                 <div className="w-full md:w-2/5 lg:w-1/2 px-4 text-center md:text-left lg:pl-12">
                     <h3 className="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
-                        No more syntax errors
+                    Customized Approach:
                     </h3>
                     <p className="sm:text-lg mt-6">
-                        LocaleData provides you easy import/export functions for your YAML files.
-                        Use a simple editor with many predefined languages to manage your locales.
-                        LocaleData also supports multiple translation types, such as simple text, plural forms,
-                        numbers, booleans, symbols, arrays, ...
+                     Our services are tailored to meet your unique needs and goals.
+                     We take the time to understand your business objectives and craft solutions 
+                     that align with your vision, ensuring maximum effectiveness and impact.
                     </p>
                 </div>
             </div>
@@ -63,11 +59,13 @@ const WhyChooseUs = () => {
                 </div>
                 <div className="w-full md:w-2/5 lg:w-1/2 px-4 md:order-first text-center md:text-left lg:pr-12">
                     <h3 className="font-bold mt-8 text-xl md:mt-0 sm:text-2xl">
-                        Bulk editing
+                    Continuous Support:
                     </h3>
                     <p className="sm:text-lg mt-6">
-                        Do you need to change the path of many translation keys at once? No problem, just
-                        use the bulk editing feature and enjoy the results.
+                     We provide ongoing support and maintenance for your 
+                    applications and software, ensuring that they remain up-to-date, secure,
+                     and optimized for performance. Our dedicated team is always available to
+                      address any issues and provide assistance whenever you need it
                     </p>
                 </div>
             </div>
